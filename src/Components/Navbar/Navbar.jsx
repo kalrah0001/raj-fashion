6ee3cce51ae68e46f3f67c410b73b0a908b13d83
Harshit Kalra <hkalra0001@gmail.com>
2024-11-20 16:33:27 +0530

    added dark mode / light mode

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,50 @@
-import React, { useContext, useState } from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
-import { Link } from 'react-router-dom'
-import { ShopContext } from '../../Context/ShopContext'
-const Navbar = () => {
-  const [menu,setMenu]=useState("shop");
-  const {getTotalCartItems}=useContext(ShopContext);
+import React, { useContext, useState } from 'react';
+import './Navbar.css';
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
+
+const Navbar = ({ theme, toggleTheme }) => {
+  const [menu, setMenu] = useState("shop");
+  const { getTotalCartItems } = useContext(ShopContext);
+
   return (
     <div className='navbar'>
-        <div className="nav-logo">
-          <img src={logo} alt="" />
-          <p>Raj Fashion</p>
-        </div>
-        <ul className="nav-menu">
-          <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration: 'none'}} to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration: 'none'}} to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration: 'none'}} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
-        </ul>
-        <div className="nav-login-cart">
+      <div className="nav-logo">
+        <img src={logo} alt="Logo" />
+        <p>Raj Fashion</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("shop")}>
+          <Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("mens")}>
+          <Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("womens")}>
+          <Link style={{ textDecoration: 'none' }} to='/womens'>Women</Link>
+          {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          <Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
         <Link to='/login'><button>Login</button></Link>
-        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-          <div className="nav-cart-count">{getTotalCartItems()}</div>
-        </div>
+        <Link to='/cart'><img src={cart_icon} alt="Cart" /></Link>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
+        <button 
+          className="theme-toggle-btn" 
+          onClick={toggleTheme}>
+          {theme === 'light' ? '🌙 Dark Mode' : '☀️ Light Mode'}
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
